d689f90ef31a66cf9ac5fd0d5612677bde1503c0
Willy Antunez <willyantunezg@gmail.com>
2023-04-14 13:11:56 -0600

    [Feat] express server created

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log('Hola mundo');
+import dotenv from 'dotenv';
+
+import { Server } from "./src/Server";
+
+dotenv.config();
+
+const server = new Server();
+
+server.listen();
---
+++ src/Server.ts
@@ -0,0 +1,43 @@
+
+import express, {Application} from 'express';
+import cors from 'cors';
+
+import { default as userRoutes } from './routes/user';
+
+export class Server {
+
+    private app:Application;
+    private port:string;
+    private apiPaths = {
+        users: '/api/users',
+    }
+
+    constructor() {
+        
+        this.app = express();
+        this.port = process.env.PORT || '8080';
+
+        this.middlewares();
+        this.routes();
+
+    }
+
+    private middlewares() {
+        this.app.use(cors());
+
+        this.app.use(express.json());
+
+        this.app.use(express.static('public'));
+    }
+
+    private routes() {
+        this.app.use(this.apiPaths.users, userRoutes)
+    }
+
+    public listen() {
+        this.app.listen(this.port, () => {
+            console.log('Server running in port 8080')
+        })
+    }
+
+}
